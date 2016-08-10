a9d6f31d39f7db816c80afc7bcfaa0a246b24303
Kinshuk Bairagi <me@kinshuk.in>
2016-08-11 04:49:06 +0530

    Simple http call

--- push.cpp
+++ push.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/Easy.hpp>
+#include <sstream>
+
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello, world!\n";
+
+  std::cout << "Hello, world!\n";
+
+  // RAII cleanup
+  curlpp::Cleanup myCleanup;
+
+  // Send request and get a result.
+  // Here I use a shortcut to get it in a string stream ...
+
+  std::ostringstream os;
+  os << curlpp::options::Url(std::string("https://raw.githubusercontent.com/Flipkart/espion/master/README.md"));
+
+  std::string asAskedInQuestion = os.str();
+
+  std::cout << asAskedInQuestion << "\n" ;
 }
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-g++ -o $1.app $1.cpp
-chmod +x $1.app
-./$1.app
